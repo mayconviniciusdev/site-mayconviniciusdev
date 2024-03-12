11c5e6ca6fc1361cde1fd8b9cb046c1852fee73a
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-12 19:01:51 -0300

    Updating family font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ['400', '600'] });
+const poppins = Poppins({ subsets: ["latin"], weight: ['500', '600', '700'] });
 
 export const metadata: Metadata = {
   title: {
