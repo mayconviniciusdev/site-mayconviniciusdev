242c1840de8c87b5a92bae2742f4d182d643dd1f
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-14 12:24:33 -0300

    Centralizing session with grid

--- src/app/content/about/about.tsx
+++ src/app/content/about/about.tsx
@@ -4,21 +4,21 @@ import { Button } from "@/components/button";
 
 export const About = () => {
   return (
-    <div className="max-w-5xl mx-auto py-32">
-      <div className="grid grid-cols-2 items-center mx-4">
-        <div className="pr-4">
+    <div className="max-w-4xl mx-auto py-32">
+      <div className="grid grid-cols-3 items-center mx-4">
+        <div className="flex justify-center">
           <Image
           src={perfil}
           quality={100}
-          style={{width: '430px', height: 'auto'}}
+          style={{width: '300px', height: 'auto'}}
           className="rounded-3xl shadow-blue-cyan shadow-2xl"
           alt="Maycon Vinicius"/>
         </div>
 
-        <div className="grid gap-y-6">
+        <div className="grid col-span-2 ml-10">
           <p className="text-blue-cyan font-bold">Olá, meu nome é</p>
-          <h2 className="text-2xl text-blue font-bold">Maycon Vinícius e sou <br/> Desenvolvedor Front-End</h2>
-          <p className="text-blue-cyan font-medium">Me encontrei no mundo da programação quando percebi os resultados e conquistas que algumas linhas de códigos podem fazer. Venho obtendo conhecimentos para o desenvolvimento web e de aplicativos com objetivo de implementar tecnologias e ajudar empresas ou projetos independentes em sua jornada online. Também sou um entusiasta por tecnologia, ideias inovadoras e um ótimo café.</p>
+          <h2 className="text-3xl text-blue font-bold">Maycon Vinícius e sou <br/> Desenvolvedor Front-End</h2>
+          <p className="text-blue-cyan font-medium py-4">Me encontrei no mundo da programação ao perceber os resultados e conquistas que algumas linhas de códigos podem desencadear. Venho obtendo conhecimentos para o desenvolvimento web e de aplicativos com o objetivo de impulsionar empresas e projetos independentes em sua jornada online. Também sou um entusiasta por tecnologia, ideias inovadoras e um ótimo café.</p>
           <div className="flex"><Button/></div>
         </div>
       </div>
