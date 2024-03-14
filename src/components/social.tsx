6f5b917be8c5609ce308bd5421239e0458f16314
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-13 22:24:10 -0300

    Adding effect hover

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -12,9 +12,9 @@ interface SocialProps {
 export const Social = ({githubIcon, linkedinIcon, instagramIcon }: SocialProps) => {
   return (
     <div className="flex justify-center gap-4 text-blue text-b">
-      <Link target="_blank" href={"https://github.com/mayconviniciusdev"}>{githubIcon && <FontAwesomeIcon icon={githubIcon} className="w-8"/>}</Link>
-      <Link target="_blank" href={"https://www.linkedin.com/in/mayconviniciusdev/"}><FontAwesomeIcon icon={linkedinIcon} className="w-8"/></Link>
-      <Link target="_blank" href={"https://www.instagram.com/mayconviniciusdev/"}><FontAwesomeIcon icon={instagramIcon} className="w-8"/></Link>
+      <Link target="_blank" href={"https://github.com/mayconviniciusdev"}>{githubIcon && <FontAwesomeIcon icon={githubIcon} className="w-8 hover:text-blue/80"/>}</Link>
+      <Link target="_blank" href={"https://www.linkedin.com/in/mayconviniciusdev/"}><FontAwesomeIcon icon={linkedinIcon} className="w-8 hover:text-blue/80"/></Link>
+      <Link target="_blank" href={"https://www.instagram.com/mayconviniciusdev/"}><FontAwesomeIcon icon={instagramIcon} className="w-8 hover:text-blue/80"/></Link>
     </div>
   );
 }
