9007bae73c4de35e450b203bce8d0ecc9cfa5d18
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-14 13:13:48 -0300

    Changing the contact button tag and preview

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Button = () => {
   return (
-    <div className="bg-blue p-2 rounded-lg">
-        <p className="text-white">ENTRE EM CONTATO</p>
+    <div className="text-sm bg-blue p-2 rounded-lg hover:bg-blue/80">
+        <button className="text-white">ENTRE EM CONTATO</button>
     </div>
   );
 }
