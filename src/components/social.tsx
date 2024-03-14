4ac6cfe8e2f4a982ee510b11fc6d34b566d1714d
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-13 22:15:40 -0300

    Creating link redirects

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import Link from "next/link";
 
 interface SocialProps {
   githubIcon?: IconProp;
@@ -11,9 +12,9 @@ interface SocialProps {
 export const Social = ({githubIcon, linkedinIcon, instagramIcon }: SocialProps) => {
   return (
     <div className="flex justify-center gap-4 text-blue text-b">
-      {githubIcon && <FontAwesomeIcon icon={githubIcon} className="w-8"/>}
-      <FontAwesomeIcon icon={linkedinIcon} className="w-8"/>
-      <FontAwesomeIcon icon={instagramIcon} className="w-8"/> 
+      <Link target="_blank" href={"https://github.com/mayconviniciusdev"}>{githubIcon && <FontAwesomeIcon icon={githubIcon} className="w-8"/>}</Link>
+      <Link target="_blank" href={"https://www.linkedin.com/in/mayconviniciusdev/"}><FontAwesomeIcon icon={linkedinIcon} className="w-8"/></Link>
+      <Link target="_blank" href={"https://www.instagram.com/mayconviniciusdev/"}><FontAwesomeIcon icon={instagramIcon} className="w-8"/></Link>
     </div>
   );
 }
