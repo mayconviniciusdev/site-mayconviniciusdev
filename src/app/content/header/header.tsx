70a5b226a103a0939053787122c76de84b62881c
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-13 22:07:08 -0300

    Updating social media icons

--- src/app/content/header/header.tsx
+++ src/app/content/header/header.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import { Button } from "@/components/button";
 import { Social } from "@/components/social";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export const Header = () => {
   return (
     <div className="my-14">
       <div className="flex justify-center gap-4 text-blue text-b">
-        <Social/>
+        <Social githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/>
       </div>
       
       <div className="mt-8 flex justify-center items-center gap-4 font-semibold text-sm text-blue-cyan cursor-pointer">
