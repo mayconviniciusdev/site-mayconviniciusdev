ac089643b228008c4cd5e8bbc90d6f3da3c66f09
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-17 14:47:50 -0300

    Updating code

--- src/app/content/header/header.tsx
+++ src/app/content/header/header.tsx
@@ -7,7 +7,7 @@ import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-
 export const Header = () => {
   return (
     <div className="my-14">
-      <div className="flex justify-center gap-4 text-blue text-b">
+      <div className="flex justify-center gap-4 text-blue">
         <Social githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/>
       </div>
       
