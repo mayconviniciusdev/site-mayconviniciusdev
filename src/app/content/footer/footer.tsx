871b51d7a9d975a73ca09589554e820f31c6ae6f
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-18 13:00:32 -0300

    Updating footer

--- src/app/content/footer/footer.tsx
+++ src/app/content/footer/footer.tsx
@@ -1,5 +1,3 @@
-import { Social } from "@/components/social"
-import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
@@ -7,10 +5,9 @@ export const Footer = () => {
   return (
     <div className="max-w-4xl mx-auto py-10 text-sm text-center">
       <div className="mx-4 text-blue-cyan font-bold">
-        <button className="">VOLTAR PARA CIMA</button>
+        <button className="flex items-center mx-auto">VOLTAR PARA CIMA <FontAwesomeIcon icon={faArrowUp} className="w-4 ml-1"/></button>
         <p className="mt-4">MAYCON VINÍCIUS, PROGRAMADOR FRONTEND. RIO GRANDE DO SUL.</p>
-        <span className="text-blue">contato@mayconviniciusdev.com</span>
-        <div className="mt-4"><Social githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/></div>
+        <p className="text-blue">contato@mayconviniciusdev.com</p>
         </div>
     </div>
   )
