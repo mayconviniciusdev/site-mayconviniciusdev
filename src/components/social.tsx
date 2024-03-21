9b1e31f1dacc79dfb8353f00b75034937f6529b7
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-03-21 00:03:05 -0300

    Adding an extra version of the social component

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -18,3 +18,13 @@ export const Social = ({githubIcon, linkedinIcon, instagramIcon}: SocialProps) =
     </div>
   );
 }
+
+export const SocialForm = ({githubIcon, linkedinIcon, instagramIcon}: SocialProps) => {
+  return (
+    <div className="flex justify-center gap-4 text-white">
+      <Link target="_blank" href={"https://github.com/mayconviniciusdev"}>{githubIcon && <FontAwesomeIcon icon={githubIcon} className="h-8 hover:text-white/80"/>}</Link>
+      <Link target="_blank" href={"https://www.linkedin.com/in/mayconviniciusdev/"}><FontAwesomeIcon icon={linkedinIcon} className="h-8 hover:text-white/80"/></Link>
+      <Link target="_blank" href={"https://www.instagram.com/mayconviniciusdev/"}><FontAwesomeIcon icon={instagramIcon} className="h-8 hover:text-white/80"/></Link>
+    </div>
+  );
+}
