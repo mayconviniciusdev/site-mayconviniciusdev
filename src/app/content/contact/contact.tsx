b233d7f1c2574ac8563b736aa10f6941a6ef92d4
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-05-06 20:41:49 -0300

    Sending contact form to email and improving validations

--- src/app/content/contact/contact.tsx
+++ src/app/content/contact/contact.tsx
@@ -9,6 +9,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form"
 
 import { ContactFormType } from "@/types/contactFormType"
+import { useRef } from "react"
+import emailjs from "@emailjs/browser"
 
 export const Contact = () => {
   const isValidPhoneNumber = (value: string) => {
@@ -16,16 +18,32 @@ export const Contact = () => {
     return phoneRegex.test(value);}
 
   const isValidMessage = (value: string) => {
-    const messageRegex = /^[a-zA-Z0-9\s]+$/;
+    const messageRegex = /^[a-zA-Z0-9\s.,!?()\[\]\-\u00C0-\u00FF]+$/;
     return messageRegex.test(value);}
   
   const ContactSchema = z.object({
   name: z.string().min(3, {message: 'Precisa de pelo menos 3 letras'}).max(20, {message: 'O máximo de letras é 20'}),
   email: z.string().email({message: 'Digite um e-mail válido...'}),
-  tel: z.string().refine(value => isValidPhoneNumber(value), {message: 'Digite um número de telefone válido...'}),
-  message: z.string().min(15, {message: 'Sua mensagem deve ser mais comprida'}).refine(value => isValidMessage(value), {message: 'A mensagem não pode conter caracteres especiais'})})
+  tell: z.string().refine(value => isValidPhoneNumber(value), {message: 'Digite um número de telefone válido...'}),
+  message: z.string().min(15, {message: 'Sua mensagem deve ser mais comprida'}).refine(value => isValidMessage(value), {message: 'A mensagem contem caracteres não aceitos'})})
 
-  const { register, formState: { errors } } = useForm<ContactFormType>({ resolver: zodResolver(ContactSchema)});
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<ContactFormType>({ resolver: zodResolver(ContactSchema)});
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail: SubmitHandler<ContactFormType> = async (data: ContactFormType) => {
+    const templateParams = {
+      name: data.name,
+      email: data.email,
+      tell: data.tell,
+      message: data.message}
+
+    if (form.current)
+    { try 
+      { const response = await emailjs.sendForm('service_49pxid1', 'template_uplm04s', form.current, 'czmTWybfk69vJ7BNb');
+      console.log('SUCESSO!', response.text);
+      reset();}
+      catch (error) { if (error instanceof Error) { console.error('FALHA...', error.message); } } };
+  }
+  
   
   return (
     <Element name="contact">
@@ -35,7 +53,7 @@ export const Contact = () => {
             <h2 className="font-bold text-white">CONTATO</h2>
             <p className="py-2 text-base text-white/80">Sinta-se a vontade de entrar em contato comigo, seja para conversar sobre suas dúvidas, para dicas de livros, séries e filmes ou até mesmo para um bate-papo sobre sua ideia ou necessidade!</p>
 
-            <form className="mt-8">
+            <form ref={form} onSubmit={handleSubmit(sendEmail)} className="mt-8">
               <input 
               {...register('name')}
               placeholder="Como você se chama?"
@@ -50,10 +68,10 @@ export const Contact = () => {
                 {errors.email && <p>{errors.email.message as string}</p>}
 
                 <input
-                {...register('tel')}
+                {...register('tell')}
                 placeholder="Agora seu Whatsapp..."
                 className="py-2 pl-4 rounded-lg bg-transparent border border-white placeholder-white focus:outline-none focus:border-white/50 focus:placeholder-white"/>
-                {errors.tel && <p>{errors.tel.message as string}</p>}
+                {errors.tell && <p>{errors.tell.message as string}</p>}
               </div>
 
               <textarea 
