9fada0c4fad92a97a5c4e63b7cf5f3d87a29efda
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-05-15 11:11:13 -0300

    Creating responsive session services

--- src/app/content/services/services.tsx
+++ src/app/content/services/services.tsx
@@ -6,23 +6,23 @@ import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-
 export const Services = () => {
   return (
     <Element name="services">
-      <div className="max-w-7xl mx-auto py-12">
-        <div className="grid gap-y-4 mx-4">
+      <div className="max-w-7xl mx-auto py-12 ">
+        <div className="mx-4 flex justify-between items-center lg:grid lg:gap-y-4 ">
           <h2 className="font-bold text-blue">MEUS SERVIÇOS</h2>
-          <div className="flex justify-start">
+          <div className="flex">
             <LinktreeBlue githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/>
           </div>
         </div>
 
-        <div className="grid grid-cols-2 mx-4 mt-12">
-          <div className="flex flex-col items-center border-r-2 border-blue-cyan/70">
+        <div className="mx-4 lg:grid lg:grid-cols-2">
+          <div className="flex flex-col border-b-2 py-10 border-blue-cyan/70 lg:items-center lg:border-r-2 lg:w-full">
             <h2 className="font-bold text-blue-cyan pb-4">Desenvolvimento de Sites</h2>
-            <p className="flex text-center text-base text-grey max-w-sm">Busco ajudar negócios, abrangendo desde o design do layout até o desenvolvimento completo do site. Especializo-me na criação de páginas institucionais e profissionais, além de landingpages, garantindo soluções personalizadas que destacam a sua marca no ambiente digital.</p>
+            <p className="flex text-base text-grey max-w-lg lg:max-w-sm lg:text-center">Busco ajudar negócios, abrangendo desde o design do layout até o desenvolvimento completo do site. Especializo-me na criação de páginas institucionais e profissionais, além de landingpages, garantindo soluções personalizadas que destacam a sua marca no ambiente digital.</p>
           </div>
           
-          <div className="flex flex-col items-center border-l-2 border-blue-cyan/70">
+          <div className="flex flex-col items-end py-10 border-t-2 border-blue-cyan/70 lg:items-center lg:border-l-2 lg:w-full">
             <h2 className="font-bold text-blue-cyan pb-4">Manutenção e suporte</h2>
-            <p className="flex text-center text-base text-grey max-w-sm">Disponibilizo serviços de manutenção e suporte técnico para assegurar a eficiência e a estabilidade de sistemas e aplicativos. Além disso, ofereço soluções completas em hospedagem e gestão de sites, garantindo que sua presença digital esteja sempre otimizada e sem interrupções.</p>
+            <p className="flex text-end text-base text-grey max-w-lg lg:max-w-sm lg:text-center">Disponibilizo serviços de manutenção e suporte técnico para assegurar a eficiência e a estabilidade de sistemas e aplicativos. Além disso, ofereço soluções completas em hospedagem e gestão de sites, garantindo que sua presença digital esteja sempre otimizada e sem interrupções.</p>
           </div>
         </div>
       </div>
