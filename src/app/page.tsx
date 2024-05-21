a54f1b8ab6439c2924bcf8e17eaa78e2fd594aa2
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-05-20 21:16:42 -0300

    Adding metadados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description:'Sou um desenvolvedor e programador web freelancer especializado ajudar empresas e projetos independentes a alcançar sucesso em sua jornada online. Entre em contato para serviços de desenvolvimento web personalizados e eficientes.',
+  keywords: ['maycon vinicius', 'desenvolvedor web', 'programador freelancer', 'desenvolvedor freelancer', 'programador web', 'contato maycon vinicius', 'desenvolvimento web'],
+  authors: [{name: 'Maycon Vinicius', url: 'https://www.linkedin.com/in/mayconviniciusdev'}],
+  robots: {index: true, follow: true},
+
+  openGraph: {
+    type: 'website',
+    url: 'https://mayconviniciusdev.com',
+    title: 'Desenvolvedor Web',
+    description: 'Tenho como objetivo implementar tecnologias e ajudar empresas ou projetos independentes em sua jornada online. Vamos trabalhar juntos?',
+    images: [
+      {url: '/foto-perfil.png',
+      width: 1200,
+      height: 630,
+      alt: 'Foto de Perfil'}
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@dmayconvinicius',
+    creator: '@dmayconvinicius'
+  },
+};
+
 import { Header } from "./header";
 import { Banner } from "./banner";
 import { About } from "./about";
