0363ff487ff6bb21f654c32fda54d004e18aee8e
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-05-20 22:03:42 -0300

    jorgefreitasdesign page

---
+++ src/app/(job)/jorgefreitasdesign/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {title: 'jorgefreitasdesign'}
+
+const Page = () => {
+  return (
+    <div>
+      Jorge
+    </div>
+  )
+}
+
+export default Page;
