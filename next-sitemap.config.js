14b0442c4049d9d54413a53235b482fa91b72e60
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-07-04 22:18:45 -0300

    Sitamp and build configurations

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.mayconviniciusdev.com',
   generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 1,
 
-  additionalPaths: [
-    { url: '/', changefreq: 'daily', priority: 1 },
-    { url: '/linktree/page', changefreq: 'weekly', priority: 0.8 },
-    { url: '/works/jorgefreitasdesign/page', changefreq: 'daily', priority: 0.5 },
-  ],
-}
+  additionalPaths: async (config) => {
+    const paths = [
+      { url: '/linktree', changefreq: 'weekly', priority: 0.8 },
+      { url: '/jorgefreitasdesign', changefreq: 'daily', priority: 0.5 },
+    ];
+    return paths;
+  },
+};
