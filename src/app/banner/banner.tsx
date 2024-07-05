fda4169acb026445f4836ae7adf7a64c7fb48d58
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-07-05 13:57:22 -0300

    Banner correction

--- src/app/banner/banner.tsx
+++ src/app/banner/banner.tsx
@@ -1,13 +1,8 @@
-'use client'
-
+'use client';
 import React from 'react';
 
 import { Slider } from '@/components/slider';
 import { SwiperProps, SwiperSlide } from "swiper/react";
-
-import Image from 'next/image';
-import banner from '../../../public/banner.png'
-import logo from '../../../public/logo.png'
 
 export const Banner = () => {
   const settings: SwiperProps = {
@@ -20,47 +15,35 @@ export const Banner = () => {
     <div>
       <Slider settings={settings}>
         <SwiperSlide>
-          <div 
-          style={{position: 'relative', width: '100%', height: '630px'}}>
-            <Image
-            src={banner}
-            alt="Banner"
-            quality={100}
-            layout="fill"
-            objectFit="cover"/>
+          <div style={{position: 'relative', width: '100%', maxHeight: '630px'}}>
+            <img
+            style={{width: '100%', height: '630px', display: 'block', objectFit: 'cover'}}
+            src="/banner.png"
+            alt="Banner"/>
             
-            <div 
-            style={{position: 'absolute', width: '150px', height: '150px',
-            top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-              <Image
-              src={logo}
+            <div style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <img
+              src='/logo.png'
               alt="Logo"
-              quality={100}
-              layout="fill"
-              objectFit="contain"/>
+              style={{maxWidth: '50%', maxHeight: '50%', height: 'auto'}}/>
             </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div 
-          style={{position: 'relative', width: '100%', height: '630px'}}>
-              <Image
-              src={banner}
-              alt="Banner"
-              quality={100}
-              layout="fill"
-              objectFit="cover"/>
+          <div style={{position: 'relative', width: '100%', height: '630px'}}>
+            <img
+            style={{width: '100%', height: '630px', display: 'block', objectFit: 'cover'}}
+            src='/banner.png'
+            alt="Banner"/>
               
-              <div 
-              style={{position: 'absolute', top: '50%',
-              left: '50%', transform: 'translate(-50%, -50%)'}}>
-                <div className=' text-white text-2xl font-bold'>
-                  <h2>Programação,</h2>
-                  <h2>Desenvolvimento e </h2>
-                  <h2>Tecnologia!</h2>
-                </div>
+            <div style={{position: 'absolute', width: '100%', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center',}}>
+              <div className='text-white text-2xl font-bold'>
+                <h2>Programação,</h2>
+                <h2>Desenvolvimento e </h2>
+                <h2>Tecnologia!</h2>
               </div>
+            </div>
           </div>
         </SwiperSlide>
       </Slider>
