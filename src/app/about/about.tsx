8237ab4c39b6145a6c66eccef7ab3340f1ae66f5
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-07-07 21:01:16 -0300

    Correcting About

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -1,8 +1,6 @@
 "use client"
-import { Element } from 'react-scroll';
 
-import Image from "next/image";
-import perfil from "../../../public/foto-perfil.png"
+import { Element } from 'react-scroll';
 import { Button } from "@/components/button";
 
 export const About = () => {
@@ -11,17 +9,15 @@ export const About = () => {
       <div className="max-w-4xl mx-auto py-32">
         <div className="mx-4 items-center md:grid grid-cols-3">
           <div className="flex justify-center">
-            <Image
-            src={perfil}
-            quality={100}
-            style={{width: '300px', height: 'auto'}}
-            className="rounded-3xl shadow-blue-cyan shadow-2xl"
+            <img
+            src="/foto-perfil.png"
+            className="w-full max-w-60 lg:max-w-lg rounded-3xl shadow-blue-cyan shadow-2xl"
             alt="Maycon Vinicius"/>
           </div>
 
           <div className="mt-16 text-center md:grid md:col-span-2 md:ml-10 md:mt-0 md:text-start">
             <p className="text-blue-cyan font-bold">Olá, meu nome é</p>
-            <h2 className="text-3xl text-blue font-bold">Maycon Vinícius e sou <br/> Desenvolvedor Front-End</h2>
+            <h2 className="text-2xl text-blue font-bold">Maycon Vinícius e sou <br/> Desenvolvedor Front-End</h2>
             <p className="text-blue-cyan font-medium py-4">Me encontrei no mundo da programação ao perceber os resultados e conquistas que algumas linhas de códigos podem desencadear. Venho obtendo conhecimentos para o desenvolvimento web e de aplicativos com o objetivo de impulsionar empresas e projetos independentes em sua jornada online. Também sou um entusiasta por tecnologia, ideias inovadoras e um ótimo café.</p>
             <div className="flex justify-center md:justify-start"><Button/></div>
           </div>
