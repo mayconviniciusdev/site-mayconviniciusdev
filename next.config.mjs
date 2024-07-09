715aa680e2c0fee9cd0850695ea47c7a647245f2
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-07-08 22:34:00 -0300

    Updating config build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
-  output: 'export'
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  async generateStaticParams() {
+    return {
+      '/': { page: '/' },
+      '/jorgefreitasdesign': { page: '/app/(job)/jorgefreitasdesign' },
+
+    };
+  },
 };
 
 export default nextConfig;
